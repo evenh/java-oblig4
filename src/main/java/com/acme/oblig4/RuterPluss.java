a8bdeca0007215932e49cb19e5fcfd8fa6275d78
Even Holthe <even.holthe@me.com>
2013-11-11 19:00:57 +0100

    Driverklassen er ferdig!

--- src/main/java/com/acme/oblig4/RuterPluss.java
+++ src/main/java/com/acme/oblig4/RuterPluss.java
@@ -3,14 +3,18 @@ package com.acme.oblig4;
 /*
  * Oblig 4 (DAPE1400) - forfattet av følgende
  * studenter fra Anvendt datateknologi, 2. året:
- * 
- * Vegar Norman <s189153@stud.hioa.no> 
+ *
+ * Vegar Norman <s189153@stud.hioa.no>
  * Per Erik Finstad <s189138@stud.hioa.no>
  * Even Holthe <s189124@stud.hioa.no>
  */
 
 public class RuterPluss {
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
+    	ReisekortSystem rs = new ReisekortSystem();
+
+        Reisekortsalg w1 = new Reisekortsalg(rs);
+        Kontrollvindu w2 = new Kontrollvindu(rs);
+        Administrasjonsvindu w3 = new Administrasjonsvindu();
     }
 }
